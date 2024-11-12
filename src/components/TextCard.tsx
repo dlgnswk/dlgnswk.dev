da8f10d251a137df15cfcbd101b8c71d01710674
ljhoon <dlwogns5140@gmail.com>
2024-11-12 22:57:18 +0900

    layout: textcard 컴포넌트 구현

---
+++ src/components/TextCard.tsx
@@ -0,0 +1,9 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+export const TextCard = () => {
+  return (
+    <Card className="w-full h-full p-4">
+      <CardContent className="text-sm text-muted-foreground"></CardContent>
+    </Card>
+  );
+};
